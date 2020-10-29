8e8d71841e92d940d2e6a7cce005a3a94824d3e3
Roksana <roksana.rebiewska@unilever.com>
2020-10-29 10:27:21 +0100

    sustainability page name change

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,17 +5,17 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\.cache\\dev-404-page.js"))),
-  "component---src-layouts-pdp-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\layouts\\pdp.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\404.js"))),
-  "component---src-pages-about-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\about.js"))),
-  "component---src-pages-brands-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\brands.js"))),
-  "component---src-pages-contact-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\contact.js"))),
-  "component---src-pages-cookie-notice-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\cookie-notice.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\index.js"))),
-  "component---src-pages-index-lipton-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\index-lipton.js"))),
-  "component---src-pages-legal-notice-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\legal-notice.js"))),
-  "component---src-pages-privacy-notice-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\privacy-notice.js"))),
-  "component---src-pages-unilever-jatkusuutliku-elu-plaan-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\unilever-jatkusuutliku-elu-plaan.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\.cache\\dev-404-page.js"))),
+  "component---src-layouts-pdp-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\layouts\\pdp.js"))),
+  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\404.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\about.js"))),
+  "component---src-pages-brands-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\brands.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\contact.js"))),
+  "component---src-pages-cookie-notice-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\cookie-notice.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\index.js"))),
+  "component---src-pages-index-lipton-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\index-lipton.js"))),
+  "component---src-pages-legal-notice-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\legal-notice.js"))),
+  "component---src-pages-privacy-notice-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\privacy-notice.js"))),
+  "component---src-pages-sustainability-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\sustainability.js")))
 }
 
