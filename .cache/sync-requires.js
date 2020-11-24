b80e66f6d54bd35987e72f5d3f84a978cf403d0d
Marcin <marcin.tuchalski@unilever.com>
2020-11-24 14:20:51 +0100

    nothing changed

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-<<<<<<< HEAD
   "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\.cache\\dev-404-page.js"))),
   "component---src-layouts-pdp-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\layouts\\pdp.js"))),
   "component---src-layouts-pdp-lipton-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\layouts\\pdp-lipton.js"))),
@@ -18,25 +17,9 @@ exports.components = {
   "component---src-pages-index-lipton-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\index-lipton.js"))),
   "component---src-pages-legal-notice-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\legal-notice.js"))),
   "component---src-pages-privacy-notice-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\privacy-notice.js"))),
+  "component---src-pages-purpose-lipton-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\purpose-lipton.js"))),
   "component---src-pages-sitemap-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\sitemap.js"))),
   "component---src-pages-sustainability-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\sustainability.js"))),
   "component---src-pages-sustainability-lipton-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\sustainability-lipton.js")))
-=======
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\.cache\\dev-404-page.js"))),
-  "component---src-layouts-pdp-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\layouts\\pdp.js"))),
-  "component---src-layouts-pdp-lipton-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\layouts\\pdp-lipton.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\404.js"))),
-  "component---src-pages-about-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\about.js"))),
-  "component---src-pages-brands-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\brands.js"))),
-  "component---src-pages-contact-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\contact.js"))),
-  "component---src-pages-cookie-notice-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\cookie-notice.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\index.js"))),
-  "component---src-pages-index-lipton-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\index-lipton.js"))),
-  "component---src-pages-legal-notice-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\legal-notice.js"))),
-  "component---src-pages-privacy-notice-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\privacy-notice.js"))),
-  "component---src-pages-purpose-lipton-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\purpose-lipton.js"))),
-  "component---src-pages-sitemap-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\sitemap.js"))),
-  "component---src-pages-sustainability-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\sustainability.js")))
->>>>>>> 9599277d2796de396a241bcc2de77d040bdcf65e
 }
 
