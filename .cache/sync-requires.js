c9a94b77d80dfb6e35c99ab20bfc69dc72d59ef7
Marcin <marcin.tuchalski@unilever.com>
2020-12-14 09:30:41 +0100

    no changes

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-<<<<<<< HEAD
   "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\.cache\\dev-404-page.js"))),
   "component---src-layouts-pdp-domestos-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\layouts\\pdp-domestos.js"))),
   "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\404.js"))),
@@ -27,6 +26,7 @@ exports.components = {
   "component---src-pages-legal-notice-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\legal-notice.js"))),
   "component---src-pages-privacy-notice-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\privacy-notice.js"))),
   "component---src-pages-products-domestos-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\products-domestos.js"))),
+  "component---src-pages-products-hellmanns-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\products-hellmanns.js"))),
   "component---src-pages-products-magnum-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\products-magnum.js"))),
   "component---src-pages-purpose-lipton-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\purpose-lipton.js"))),
   "component---src-pages-sitemap-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\sitemap.js"))),
@@ -34,36 +34,5 @@ exports.components = {
   "component---src-pages-sustainability-hellmanns-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\sustainability-hellmanns.js"))),
   "component---src-pages-sustainability-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\sustainability.js"))),
   "component---src-pages-sustainability-lipton-js": hot(preferDefault(require("C:\\Users\\marcin.tuchalski\\Desktop\\baltic\\general-site\\src\\pages\\sustainability-lipton.js")))
-=======
-  "component---cache-dev-404-page-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\.cache\\dev-404-page.js"))),
-  "component---src-layouts-pdp-domestos-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\layouts\\pdp-domestos.js"))),
-  "component---src-pages-404-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\404.js"))),
-  "component---src-pages-about-domestos-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\about-domestos.js"))),
-  "component---src-pages-about-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\about.js"))),
-  "component---src-pages-about-magnum-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\about-magnum.js"))),
-  "component---src-pages-blog-domestos-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\blog-domestos.js"))),
-  "component---src-pages-brands-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\brands.js"))),
-  "component---src-pages-contact-domestos-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\contact-domestos.js"))),
-  "component---src-pages-contact-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\contact.js"))),
-  "component---src-pages-cookie-notice-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\cookie-notice.js"))),
-  "component---src-pages-delivery-magnum-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\delivery-magnum.js"))),
-  "component---src-pages-history-hellmanns-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\history-hellmanns.js"))),
-  "component---src-pages-index-domestos-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\index-domestos.js"))),
-  "component---src-pages-index-hellmanns-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\index-hellmanns.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\index.js"))),
-  "component---src-pages-index-lipton-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\index-lipton.js"))),
-  "component---src-pages-index-magnum-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\index-magnum.js"))),
-  "component---src-pages-legal-notice-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\legal-notice.js"))),
-  "component---src-pages-privacy-notice-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\privacy-notice.js"))),
-  "component---src-pages-products-domestos-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\products-domestos.js"))),
-  "component---src-pages-products-hellmanns-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\products-hellmanns.js"))),
-  "component---src-pages-products-magnum-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\products-magnum.js"))),
-  "component---src-pages-purpose-lipton-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\purpose-lipton.js"))),
-  "component---src-pages-sitemap-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\sitemap.js"))),
-  "component---src-pages-sustainability-domestos-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\sustainability-domestos.js"))),
-  "component---src-pages-sustainability-hellmanns-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\sustainability-hellmanns.js"))),
-  "component---src-pages-sustainability-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\sustainability.js"))),
-  "component---src-pages-sustainability-lipton-js": hot(preferDefault(require("C:\\Users\\roksana.rebiewska\\Desktop\\project\\Baltics\\general-site\\src\\pages\\sustainability-lipton.js")))
->>>>>>> efbf312a789606446983a9d0e5a77cf67c54a66c
 }
 
