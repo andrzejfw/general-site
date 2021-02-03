9c2a836c6a982f6b9b3eb008b3a3467e3cfa737a
Roksana <roksana.rebiewska@unilever.com>
2021-02-03 08:12:51 +0100

    product categories in mobile headers fix

--- src/components/HeaderDomestos/HeaderBurgerDomestos.js
+++ src/components/HeaderDomestos/HeaderBurgerDomestos.js
@@ -23,6 +23,14 @@ function hideOnClick() {
   document.querySelector("#productCat").classList.remove("show");
 }
 
+function showArrow() {
+  if (document.querySelector("#productCat").classList == "show") {
+    document.querySelector("#arrow").innerHTML = "&#9652;";
+  } else {
+    document.querySelector("#arrow").innerHTML = "&#9662;";
+  }
+}
+
   return (
     <nav role="navigation">
     <div id="menuToggle">
@@ -30,7 +38,7 @@ function hideOnClick() {
     <div class="hamburger"><div></div></div>
     <a href="/domestos"><img src={DomestosLogo} alt="Domestos Eesti WC-puhastusvahendid logo"/></a>
     <ul id="menu">
-      <li><div id="uncheckAll" onClick={() => {showProducts();}} activeClassName="current">Tooted &nbsp; &#10095;</div></li>
+      <li><div id="uncheckAll" onClick={() => {showProducts(); showArrow()}} activeClassName="current">Tooted <span id="arrow">&#9662;</span></div></li>
 
       <div id="productCat">
       <li><Link id="uncheckAll" onClick={() =>{uncheckAll('menuToggle');hideOnClick()}} activeClassName="current" to="/domestos/products">All</Link></li>
